35d7f768e1e4c6090fc695d4e02862f9536da61b
Murilo <murilopaulo051@gmail.com>
2025-10-13 19:55:27 -0300

    atulização 2.2

--- js/informativo.js
+++ js/informativo.js
@@ -7,12 +7,14 @@ const periodo_informativo = document.getElementById('periodo_informativo')
 for(let i = 0; i < localStorage.length; i++) {
     if(localStorage.key(i) == 'User_agendamento') {
         const agendamento = JSON.parse(localStorage.getItem(localStorage.key(i)))
+
         nome_informativo.textContent = agendamento.nome
         email_informativo.textContent = agendamento.email
         tel_informativo.textContent = agendamento.telefone
         periodo_informativo.textContent = agendamento.periodo
         let dataFormatada = new Date(agendamento.data).toLocaleDateString('pt-BR')
         data_informativo.textContent = dataFormatada
+        
         console.log(agendamento)
     }
 }
